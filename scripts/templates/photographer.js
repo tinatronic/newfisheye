50a8ceb0b76607de04f83447cbed156010d3c36a
Justine Patoux <justine.patoux@gmail.com>
2023-07-24 10:40:52 +0200

    Added innerhtml elements of the usercard

--- scripts/templates/photographer.js
+++ scripts/templates/photographer.js
@@ -1,17 +1,26 @@
 function photographerTemplate(data) {
-    const { name, portrait } = data;
-
+    const { name, id, city, country, tagline, price, portrait } = data;
+    console.log(name);
     const picture = `assets/photographers/${portrait}`;
-
+    
     function getUserCardDOM() {
-        const article = document.createElement( 'article' );
-        const img = document.createElement( 'img' );
-        img.setAttribute("src", picture)
-        const h2 = document.createElement( 'h2' );
-        h2.textContent = name;
-        article.appendChild(img);
-        article.appendChild(h2);
-        return (article);
+        const article = document.createElement('article');
+        
+        article.innerHTML = `<a href="#">
+                                <img src="../assets/photographers/${portrait}" alt="photographer portrait">
+                                <h2>${name}</h2>
+                             </a>
+                             <p>${city}, ${country}</p>
+                             <p>${tagline}</p>
+                             <p>${price}€/jour</p>`
+        // const img = document.createElement( 'img' );
+        // img.setAttribute("src", picture)
+        // const h2 = document.createElement( 'h2' );
+        // h2.textContent = name;
+        // article.appendChild(img);
+        // article.appendChild(h2);
+        
+        return article;
     }
     return { name, picture, getUserCardDOM }
 }
