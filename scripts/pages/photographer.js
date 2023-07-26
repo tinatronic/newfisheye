0b0bff1fe7093dc8e9fed6299062995e61b8e107
Justine Patoux <justine.patoux@gmail.com>
2023-07-26 11:32:21 +0200

    Implemented media list in the DOM

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,3 +1,4 @@
+//Get the value of the ID parameter of the URL
 const url = new URLSearchParams(document.location.search);
 const photographerIdURL = parseInt(url.get("id"));
 
@@ -6,20 +7,20 @@ async function getPhotographers() {
         const res = await fetch('../data/photographers.json');
         const data = await res.json();
         const photographers = data.photographers;
-        //console.log(photographers);
+
         return photographers;
     } catch(err) {
         throw new Error("Error fetching data.");
-    }
-    
+    }   
 }
-// Get the photographer corresponsing of the Id of then page
+
+// Get the photographer corresponsing of the Id of the page
 async function getPhotographerbyId() {
     try {
         const res = await fetch('../data/photographers.json');
         const data = await res.json();
         const photographerById = data.photographers.find((dataPhotographer) => dataPhotographer.id === photographerIdURL);
-        //console.log(photographerById);
+
         return photographerById;
     } catch(err) {
         throw new Error("Error fetching data.");
@@ -31,23 +32,36 @@ async function getMediabyId() {
     try {
         const res = await fetch('../data/photographers.json')
         const data = await res.json();
-        const photographerMediaById = data.media.filter((dataMediaPhotographer) => dataMediaPhotographer.photographerId === photographerIdURL );
-        console.log(photographerMediaById)
+        const photographerMediaById = data.media.filter((dataMediaPhotographer) => dataMediaPhotographer.photographerId === photographerIdURL);
+
         return photographerMediaById
     } catch(err) {
         throw new Error("Error fetching data.");
     }
 }
 
+// Get the header template and display the data on the page
 async function displayPhotographerHeader(photographerById) {
     const headerModel = photographerTemplate(photographerById);
     headerModel.displayHeaderDOM();
 }
 
+// Get the media template and display the data on the page
+async function displayMedia(photographerMediaById) {
+    const mediaSection = document.getElementById("media"); 
+
+    photographerMediaById.forEach((photographerMediaById) => {
+        const mediaModel = mediaTemplate(photographerMediaById);
+        const mediaCardDOM = mediaModel.displayMediaCardDOM();
+        mediaSection.appendChild(mediaCardDOM);
+    });
+}
+
 async function init() {
     const photographertoDisplay = await getPhotographerbyId();
-    //console.log(photographertoDisplay);
+    const mediatoDisplay = await getMediabyId();
     displayPhotographerHeader(photographertoDisplay);
+    displayMedia(mediatoDisplay);
 }
 
 init();
--- scripts/templates/media.js
+++ scripts/templates/media.js
@@ -1,13 +1,22 @@
-console.log("Toto")
-function mediaTemplate(data) {
-    const { id, photographerId, title, image, video, likes, date, price, name } = data;
-
+function mediaTemplate(media) {
+    const { id, title, image, video, likes } = media;
+    
     function displayMediaCardDOM() {
         const mediaArticle = document.createElement('article');
-        mediaArticle.classList('media-card');
 
-        const mediaContent = `<figure>
-                                <img src="../assets/media/${photographerId}/${image}">
+        // Create the media figure and check if the media is an img or a video
+        const mediaContent = `<figure class="media-card">
+                                ${media.hasOwnProperty("image")
+                                ? `<a href="#" class="media-link"><img src="../assets/media/${image}" class="media-content id-${id}" alt="${title}"></img></a>`
+                                : media.hasOwnProperty("video")
+                                ? `<a href="#" class="media-link"><video src="../assets/media/${video}" class="media-content id-${id}" alt="${title}"></video></a>`
+                                : ""}
+                                <figcaption class="media-description">${title}
+                                    <div>
+                                        <span>${likes}</span>
+                                        <i class="fas fa-heart" aria-label="likes"></i>
+                                    </div>
+                                </figcaption>
                             </figure>`
 
         mediaArticle.innerHTML = mediaContent;
