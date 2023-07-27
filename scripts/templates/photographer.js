23de5ae02d3f63f432bf0d229fe8b02390b5ea92
Justine Patoux <justine.patoux@gmail.com>
2023-07-27 21:17:47 +0200

    nothing works!

--- scripts/templates/photographer.js
+++ scripts/templates/photographer.js
@@ -2,19 +2,20 @@ function photographerTemplate(data) {
     const { name, id, city, country, tagline, price, portrait } = data;
     const picture = `assets/photographers/${portrait}`;
     const link = `./photographer.html?id=${id}`;
-
+    
+    // Creates the HTML structure of the photographers cards
     function displayCardDOM() {
         const article = document.createElement('article');
         article.classList.add("photographer");
         
         const photographerCard = `<a href="${link}" aria-label="navigate to the photographer page" tabindex="0">
-                                    <img class="photographer__portrait" src="${picture}" alt="photographer portrait">
-                                    <h2>${name}</h2>
-                                  </a>
-                                  <p class="photographer__location">${city}, ${country}</p>
-                                  <p class="photographer__tagline">${tagline}</p>
-                                  <p class="photographer__price">${price}€/jour</p>`
-
+        <img class="photographer__portrait" src="${picture}" alt="photographer portrait">
+        <h2>${name}</h2>
+        </a>
+        <p class="photographer__location">${city}, ${country}</p>
+        <p class="photographer__tagline">${tagline}</p>
+        <p class="photographer__price">${price}€/jour</p>`
+        
         article.innerHTML = photographerCard;
         // const img = document.createElement( 'img' );
         // img.setAttribute("src", picture)
@@ -25,17 +26,18 @@ function photographerTemplate(data) {
         
         return article;
     }
-
+    
+    // Creates the HTML structure of the photographer header
     function displayHeaderDOM() {
         const photographerHeader = document.querySelector(".photographer__header");
         const headerContent = `<div class="photographer__info">
-                                <h1 class="info-name">${name}</h1>
-                                <p class="info-location">${city}, ${country}</p>
-                                <p class="info-tagline">${tagline}</p>
-                            </div>
-                            <button class="contact_button" onclick="displayModal()">Contactez-moi</button>
-                            <img class="photographer__portrait" src="${picture}" alt="${name}">`
-
+        <h1 class="info-name" tabindex="0">${name}</h1>
+        <p class="info-location" tabindex="0">${city}, ${country}</p>
+        <p class="info-tagline" tabindex="0">${tagline}</p>
+        </div>
+        <button id="open-btn" type="button" class="contact_button open-btn" aria-label="Contact me" tabindex="0" onclick="displayModal()">Contactez-moi</button>
+        <img class="photographer__portrait" src="${picture}" alt="${name}">`
+        
         photographerHeader.innerHTML = headerContent;
         return photographerHeader;
     }
--- scripts/utils/contactForm.js
+++ scripts/utils/contactForm.js
@@ -1,9 +1,100 @@
+// DOM elements
+const modal = document.getElementById("contact_modal");
+//const openBtn = document.querySelector(".open-btn");
+const closeBtn = document.querySelector(".close-btn")
+const formContact = document.getElementById("contact-form");
+const formData = document.querySelectorAll(".form-data");
+const firstName = document.getElementById("first-name");
+const lastName = document.getElementById("last-name");
+const email = document.getElementById("email");
+
+
+// Opens the modal
 function displayModal() {
-    const modal = document.getElementById("contact_modal");
-	modal.style.display = "block";
+    modal.style.display = "block";
+
+    //$mainWrapper.attr('aria-hidden', 'true')
+    modal.attr('aria-hidden', 'false')
+    body.addClass('no-scroll')
+    //modal.css('display', 'flex')
+    closeBtn.focus()
+    modal.focus();
+}
+    
+// Closes modal and resets form
+function closeModal() {
+    modal.style.display = "none";
+    formContact.reset();
+        
+    // Allows to close modal with 'Escape' key -- NOT WORKING --
+    document.addEventListener("keyup", function (event) {
+        if (event.key === 27) {
+            console.log("Close the modal")
+            closeModal();
+        }
+    });
 }
 
-function closeModal() {
-    const modal = document.getElementById("contact_modal");
-    modal.style.display = "none";
+// Checks if conditions are met to validate form
+function validateForm(event) {
+    event.preventDefault();
+    event.stopPropagation();
+    const conditions = [
+    validateFirstName(),
+    validateLastName(),
+    validateEmail(),
+    validateConditions()
+    ]
+    if (conditions.filter((cond) => !cond).length) {
+        return false;
+    }
 }
+    
+// Regex for fist name, last name & email inputs
+const nameRegex = /^[A-zÀ-ÿ]{2,25}$/;
+const emailRegex = /^[a-zA-Z][a-zA-Z0-9\-\_\.]+@[a-zA-Z0-9]{2,}\.[a-zA-Z0-9]{2,}$/;
+    
+// Checks if first name input is valid
+function validateFirstName() {
+    //const regexFirstName = /^[A-zÀ-ÿ]{2,25}$/;
+    const parent = firstName.parentNode;
+        
+    if (firstName.value == "" || !nameRegex.test(firstName.value)) {
+        parent.setAttribute("data-error", "Veuillez entrer 2 caractères ou plus pour le champ du prénom.");
+        parent.setAttribute("data-error-visible", "true");
+        return false;
+    }
+        
+    parent.setAttribute("data-error-visible", "false");
+    return true;
+}
+    
+    // Checks if last name input is valid
+    function validateLastName() {
+        //const regexLastName = /^[A-zÀ-ÿ]{2,25}$/;
+        const parent = lastName.parentNode;
+        
+        if (lastName.value == "" || !nameRegex.test(lastName.value)) {
+            parent.setAttribute("data-error", "Veuillez entrer 2 caractères ou plus pour le champ du nom.");
+            parent.setAttribute("data-error-visible", "true");
+            return false;
+        }
+        
+        parent.setAttribute("data-error-visible", "false");
+        return true;
+    }
+    
+    // Checks if email input is valid
+    function validateEmail() {
+        //const regexEmail =
+        ///^[a-zA-Z][a-zA-Z0-9\-\_\.]+@[a-zA-Z0-9]{2,}\.[a-zA-Z0-9]{2,}$/;
+        const parent = document.getElementById("email").parentNode;
+        
+        if (email.value == "" || !emailRegex.test(email.value)) {
+            parent.setAttribute("data-error", "Veuillez saisir une adresse e-mail valide.");
+            parent.setAttribute("data-error-visible", "true");
+            return false;
+        }
+        parent.setAttribute("data-error-visible", "false");
+        return true;
+    }
