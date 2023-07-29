4ed6d566871411493ffa6e29da273a8828b94132
Justine Patoux <justine.patoux@gmail.com>
2023-07-29 11:47:06 +0200

    fixed validateform & added consolelog data

--- scripts/utils/contactForm.js
+++ scripts/utils/contactForm.js
@@ -1,6 +1,6 @@
 // DOM elements
 const modal = document.getElementById("contact_modal");
-const openBtn = document.querySelector(".open-btn");
+const openBtn = document.querySelector("#open-btn");
 const closeBtn = document.querySelector(".close-btn")
 const formContact = document.getElementById("contact-form");
 const formData = document.querySelectorAll(".form-data");
@@ -12,17 +12,34 @@ const message = document.getElementById('message');
 // Listener that triggers the displayModal function
 openBtn.addEventListener("click", displayModal);
 
+openBtn.addEventListener("keydown", (e) => {
+    if (e.key === 'Enter') {
+        displayModal();
+    }
+});
+
 // Opens the modal
 function displayModal() {
     modal.style.display = "block";
     modal.focus();
+    document.body.classList.add("no-scroll");
 }
     
 // Closes modal and resets form
 function closeModal() {
     modal.style.display = "none";
     formContact.reset();
+    document.body.classList.remove("no-scroll");
 }
+
+document.addEventListener("keydown", (e) => {
+    if (e.key === 'Escape') {
+        closeModal();
+    }
+});
+
+// Validate form
+document.querySelector("#submit").addEventListener("click", validateForm);
 
 // Checks if conditions are met to validate form
 function validateForm(event) {
@@ -37,6 +54,8 @@ function validateForm(event) {
     if (conditions.filter((cond) => !cond).length) {
         return false;
     }
+    returnInputValue();
+    closeModal();
 }
     
 // Regex for fist name, last name & email inputs
@@ -45,7 +64,6 @@ const emailRegex = /^[a-zA-Z][a-zA-Z0-9\-\_\.]+@[a-zA-Z0-9]{2,}\.[a-zA-Z0-9]{2,}
     
 // Checks if first name input is valid
 function validateFirstName() {
-    //const regexFirstName = /^[A-zÀ-ÿ]{2,25}$/;
     const parent = firstName.parentNode;
 
     if (firstName.value == "" || !nameRegex.test(firstName.value)) {
@@ -60,7 +78,6 @@ function validateFirstName() {
     
 // Checks if last name input is valid
 function validateLastName() {
-    //const regexLastName = /^[A-zÀ-ÿ]{2,25}$/;
     const parent = lastName.parentNode;
         
     if (lastName.value == "" || !nameRegex.test(lastName.value)) {
@@ -73,10 +90,8 @@ function validateLastName() {
     return true;
 }
     
-    // Checks if email input is valid
+// Checks if email input is valid
 function validateEmail() {
-    //const regexEmail =
-    ///^[a-zA-Z][a-zA-Z0-9\-\_\.]+@[a-zA-Z0-9]{2,}\.[a-zA-Z0-9]{2,}$/;
     const parent = document.getElementById("email").parentNode;
         
     if (email.value == "" || !emailRegex.test(email.value)) {
@@ -88,6 +103,7 @@ function validateEmail() {
     return true;
 }
 
+// Checks if message input is valid
 function validateMessage() {
     const parent = document.getElementById('message').parentNode;
 
@@ -98,4 +114,17 @@ function validateMessage() {
     }
     parent.setAttribute("data-error-visible", "false");
     return true;
+}
+
+// Gets and onsole.logs the value of the inputs
+function returnInputValue() {
+    const firstNameValue = firstName.value;
+    const lastNameValue = lastName.value;
+    const emailValue = email.value;
+    const messageValue = message.value;
+
+    console.log("First name :", firstNameValue);
+    console.log("Last name :", lastNameValue);
+    console.log("Email :", emailValue);
+    console.log("Message :", messageValue);
 }
